e2d3229fe4672a85b2aefa49d4d1f55efb92f064
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-06-17 15:16:56 +0200

    Added ceiling led

--- src/buttons.js
+++ src/buttons.js
@@ -1,5 +1,7 @@
 
 const url = document.URL + '/api';
+const urlSign = url + "/sign";
+const urlCeiling = url + "/roof";
 
 var setColor = function (rgb) {
     var r = Number(rgb.substring(4, rgb.length - 1).split(",")[0])
@@ -9,35 +11,35 @@ var setColor = function (rgb) {
     document.getElementById("colorInput").value = hex;
 }
 
-function on() {
-    sendRequest("on", "true");
+function on(destination) {
+    sendRequest("on", "true",destination);
     document.getElementById("on").disabled = true;
     document.getElementById("off").disabled = false;
 }
 
-function off() {
-    sendRequest("on", "false");
+function off(destination) {
+    sendRequest("on", "false",destination);
     document.getElementById("off").disabled = true;
     document.getElementById("on").disabled = false;
 }
 
-function text() {
-    sendRequest("text", document.getElementById("textInput").value);
+function text(destination) {
+    sendRequest("text", document.getElementById("textInput").value,destination);
 }
 
-function speed() {
-    sendRequest("speed", 55-document.getElementById("speedInput").value);
+function speed(destination) {
+    sendRequest("speed", 55 - document.getElementById("speedInput").value,destination);
 }
 
-function animation() {
-    sendRequest("animationType", document.getElementById("selector").value);
+function animation(destination) {
+    sendRequest("animationType", document.getElementById("selector").value,destination);
 }
 
-function emptyTicks() {
-    sendRequest("emptyTicks", document.getElementById("emptyTicks").value);
+function emptyTicks(destination) {
+    sendRequest("emptyTicks", document.getElementById("emptyTicks").value,destination);
 }
 
-function color() {
+function color(destination) {
     element = document.getElementById("colorInput").value;
 
     splitter = element.split("#")[1];
@@ -52,7 +54,7 @@ function color() {
 
     output = pad(rP, 3) + pad(gP, 3) + pad(bP, 3);
 
-    sendRequest("color", output);
+    sendRequest("color", output,destination);
 }
 
 window.onload = function () {
@@ -63,8 +65,8 @@ window.onload = function () {
             var json = JSON.parse(http.responseText);
             console.log(json);
             document.getElementById("textInput").value = json.text;
-            document.getElementById("speedInput").value = 55-json.speed;
-	    document.getElementById("selector").value  = json.animationType;
+            document.getElementById("speedInput").value = 55 - json.speed;
+            document.getElementById("selector").value = json.animationType;
             setColor(json.color);
             if (json.isActive === 1) {
                 document.getElementById("on").disabled = true;
@@ -78,14 +80,24 @@ window.onload = function () {
     http.send();
 }
 
-function sendRequest(param, value) {
+function sendRequest(param, value, destination="none") {
     http = new XMLHttpRequest();
-    http.open("POST", url + "?" + param + "=" + value);
+    requestURL = "";
+    if (destination == "sign") {
+        requestURL = urlSign + "?" + param + "=" + value;
+    } else if (destination == "ceiling") {
+        requestURL = urlCeiling + "?" + param + "=" + value;
+    } else {
+        console.log("Uknown destination ("+destination+")");
+        return;
+    }
+    http.open("POST", requestURL);
+
     http.onreadystatechange = function () {
         if (http.readyState === 4 && http.status === 200) {
             showToast("Successfully updated!");
         } else if (http.readyState === 4) {
-            showToast("Request failed! (Code: "+http.status+")");
+            showToast("Request failed! (Code: " + http.status + ")");
         }
     };
     http.send();
